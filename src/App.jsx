d6f5b87ec2980d10f0e1555069d61acb46cbc70e
ESSARHTHUMAR <essarthumar177@gmail.com>
2023-12-26 14:46:35 +0530

    Vercel ThemeBtn Error Fix.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Components/Card";
-import ThemeBtn from "./Components/themeBtn";
+import ThemeBtn from "./Components/ThemeBtn"
 import { ThemeProvider } from "./context/theme";
 
 function App() {
@@ -28,7 +28,7 @@ function App() {
         <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
             <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-              <ThemeBtn/>
+              <ThemeBtn />
             </div>
 
             <div className="w-full max-w-sm mx-auto">
